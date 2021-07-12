1f5dc4c6796abe63fef2268d18ae798789b5fae6
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-12 16:04:44 -0300

    requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
-import * as api from './services/api';
 
 class App extends React.Component {
   render() {
-    api.getProductsFromCategoryAndQuery("MLB5672", null).then((r) => console.log(r.results));
     return (<p></p>);
   }
 }
