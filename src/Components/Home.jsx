247754b68169f771aeba1c7a74c2fa6eb39ba499
Rafael Primon <rafaelfernandesprimon@gmail.com>
2021-07-12 19:33:36 -0300

    req 05 - cria componente Product e ProductFilter e add logica+ func no componente home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductFilter from './ProductFilter';
+
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      products: [],
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onChange({ target: { value } }) {
+    this.setState({
+      search: value,
+    });
+  }
+
+  async onClick() {
+    const { search } = this.state;
+    const products = await getProductsFromCategoryAndQuery('$categoryId', `$${search}`);
+    const { results } = products;
+    this.setState({
+      products: results,
+      search: '',
+    });
+  }
+
   render() {
+    const { products, search } = this.state;
     return (
       <div>
         <header className="header">
@@ -17,10 +47,22 @@ class Home extends React.Component {
               name="seachText"
               className="searchInput"
               placeholder="Search"
+              data-testid="query-input"
+              id="search-bar"
+              value={ search }
+              onChange={ this.onChange }
             />
             Digite algum termo de pesquisa ou escolha uma categoria.
           </label>
+          <button
+            data-testid="query-button"
+            onClick={ this.onClick }
+            type="button"
+          >
+            Buscar
+          </button>
         </header>
+        <ProductFilter products={ products } />
       </div>
     );
   }
