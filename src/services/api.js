80bfa5351b8c18144bbb86de1434ad3b060dbf8f
MaxAlves0035 <maxalves2@hotmail.com>
2021-07-12 15:34:35 -0300

    Cria a funçao getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
+// Cria uma função onde traz as categorias do mercadolibre
 export async function getCategories() {
-  // Implemente aqui
+  const apiCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
+
+  return fetch(apiCategories)
+    .then((result) => result.json())
+    .then((response) => response);
 }
+// Retorna uma promise das categorias do mercadolibre
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
