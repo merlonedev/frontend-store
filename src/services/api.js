84758737eeb66c5134b235e9befa24ebb253b16f
ladcs <hpladcs@gmail.com>
2021-07-12 16:57:03 -0300

    R1| API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URLCategories = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const fetchCategories = await fetch(URLCategories);
+  const fetchCategoriesJson = await fetchCategories.json();
+  return fetchCategoriesJson;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URLMLB = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const fetchProduct = await fetch(URLMLB);
+  const fetchProductJson = await fetchProduct.json();
+  return fetchProductJson;
 }
