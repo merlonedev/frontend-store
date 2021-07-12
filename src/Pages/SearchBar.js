40d168f02e0df481c6224657b6455feefc714061
jmlinger <jmlinger10@gmail.com>
2021-07-12 17:43:16 -0300

    Resolse requisito 4: Corrigiu-se erros no lint

--- src/Pages/SearchBar.js
+++ src/Pages/SearchBar.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react';
 import { getCategories } from '../services/api';
 
 class SearchBar extends Component {
-  constructor () {
-    super()
-    
+  constructor() {
+    super();
+
     this.state = {
-      categories: [], 
-    }
+      categories: [],
+    };
 
     this.fetchCategories = this.fetchCategories.bind(this);
   }
 
   componentDidMount() {
-    this.fetchCategories()
+    this.fetchCategories();
   }
 
   async fetchCategories() {
-    const categories = await getCategories()
-    this.setState ({
-      categories: categories,
-    })
-     // console.log(name)
-  } 
+    const categories = await getCategories();
+    this.setState({
+      categories,
+    });
+  }
 
   render() {
     const { categories } = this.state;
@@ -35,9 +34,16 @@ class SearchBar extends Component {
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
-        <aside> 
+        <aside>
           <ul>
-            { categories.map((categorie) => <li data-testid="category" key={ categorie.name }> { categorie.name } </li>) }
+            { categories.map((categorie) => (
+              <li
+                data-testid="category"
+                key={ categorie.name }
+              >
+                { categorie.name }
+              </li>
+            ))}
           </ul>
         </aside>
       </div>
