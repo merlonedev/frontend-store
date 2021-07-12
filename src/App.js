f81ef4434c01db9b203974d391fabdc4f528914a
Luis Fernando Fonseca <luisffg91@gmail.com>
2021-07-12 18:47:24 -0300

    Criterio 03 aprovado no teste e no lint

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
       </BrowserRouter>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <header>
+          <Link to="/ShoppingCart" data-testid="shopping-cart-button">
+            Carrinho
+          </Link>
+          <SearchBar />
+        </header>
       </div>
     );
   }
