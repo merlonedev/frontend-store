d24a83f3cf3c385cfae25ea1bc83c04b0f919a65
Euller Braz <euller.braz99@gmail.com>
2021-07-12 16:18:10 -0300

    Adicionado Pagina Home a app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import {} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>Edit src/App.js and save to reload.</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Route path="/" component={ Home } />
+      </BrowserRouter>
     );
   }
 }
