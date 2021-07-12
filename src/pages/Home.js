81a0baa47c8d7343c96af8f39352754a219f530c
Matheus Gonçalves <55060810+matheusgbl@users.noreply.github.com>
2021-07-12 19:25:08 -0300

    Correções aplicadas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,62 @@
 import React from 'react';
+import * as api from '../services/api';
+import Products from './Products';
 import CartButton from '../Components/CartButton';
 import Category from '../Components/Categorias';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      categoryId: '',
+      productList: [],
+    };
+    this.inputList = this.inputList.bind(this);
+    this.requestProducts = this.requestProducts.bind(this);
+  }
+
+  inputList({ target }) {
+    this.setState({
+      search: target.value,
+    });
+  }
+
+  requestProducts() {
+    const { categoryId, search } = this.state;
+    api.getProductsFromCategoryAndQuery(categoryId, search)
+      .then(({ results }) => (
+        this.setState({
+          productList: results,
+        })
+      ));
+  }
+
   render() {
+    const { productList } = this.state;
     return (
-      <header>
-        <label htmlFor="searchBar" data-testid="home-initial-message">
-          <input name="searchBar" type="text" />
+      <form>
+        <p
+          data-testid="home-initial-message"
+        >
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </label>
+        </p>
+        <input
+          type="text"
+          data-testid="query-input"
+          onChange={ this.inputList }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.requestProducts }
+        >
+          Clique Aqui
+        </button>
+        <Products productList={ productList } />
         <CartButton />
         <Category />
-      </header>
+      </form>
     );
   }
 }
