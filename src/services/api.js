fe35cd1d3cf82f5c14cf39a6b689535384f8285a
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-12 20:42:30 -0300

    Requisito 5

--- src/services/api.js
+++ src/services/api.js
@@ -5,9 +5,13 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const api = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
-  );
-  const categoryAndQuery = api.json();
-  return categoryAndQuery;
+  let URL;
+  if (!categoryId) {
+    URL = `https://api.mercadolibre.com/sites/MLB/search?category=${''}&q=${query}`;
+  } else if (categoryId) {
+    URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  }
+  const response = await fetch(URL);
+  const resolved = await response.json();
+  return resolved;
 }
