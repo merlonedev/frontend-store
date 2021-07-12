da3ef820de64e8c6629b5ff6642f61c4c4d0c201
jmlinger <jmlinger10@gmail.com>
2021-07-12 17:26:29 -0300

    Resolse requisito 4: criou-se uma lista com os nomes das categorias

--- src/Pages/SearchBar.js
+++ src/Pages/SearchBar.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class SearchBar extends Component {
+  constructor () {
+    super()
+    
+    this.state = {
+      categories: [], 
+    }
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories()
+  }
+
+  async fetchCategories() {
+    const categories = await getCategories()
+    this.setState ({
+      categories: categories,
+    })
+     // console.log(name)
+  } 
+
   render() {
+    const { categories } = this.state;
+
     return (
       <div>
         <input
@@ -10,6 +35,11 @@ class SearchBar extends Component {
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <aside> 
+          <ul>
+            { categories.map((categorie) => <li data-testid="category" key={ categorie.name }> { categorie.name } </li>) }
+          </ul>
+        </aside>
       </div>
     );
   }
