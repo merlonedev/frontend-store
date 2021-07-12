5a41293e2226bd3370d91f7bed72be0c2ff198ab
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-12 16:54:38 -0300

    implementa getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,12 @@
+
 export async function getCategories() {
-  // Implemente aqui
-}
+  const categories_api = "https://api.mercadolibre.com/sites/MLB/categories";
+
+  return fetch(categories_api)
+    .then((result) => result.json())
+    .then((response) => response);
+    console.log(result);
+  }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
