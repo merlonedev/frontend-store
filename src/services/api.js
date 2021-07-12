09569208bbe4813bd39b8d4e500a85fa8dbd505a
LuanAlexandre <luanrb19@gmail.com>
2021-07-12 15:27:05 -0400

    main-group-26-fix-requisito1

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,26 @@
 export async function getCategories() {
   // Implemente aqui
   const requestApi = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  return requestApi.results;
+  const categories = await requestApi.json();
+  return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   let requestApi;
+  let categories;
   if (categoryId === undefined) {
     requestApi = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$QUERY');
-    return requestApi.results;
+    categories = await requestApi.json();
+    return categories;
   }
   if (query === undefined) {
     requestApi = await fetch('https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID');
-    return requestApi.results;
+    categories = await requestApi.json();
+    return categories;
   }
 
   requestApi = await fetch('https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY');
-  return requestApi.results;
+  categories = await requestApi.json();
+  return categories;
 }
