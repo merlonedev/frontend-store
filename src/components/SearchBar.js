49b7e88fad014041c7bcdc274cbae873886ef307
rachidb5 <jordan.rachid@gmail.com>
2021-07-12 19:25:41 -0300

    Carrinho de compra

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import cartImage from '../assets/carro.svg'
+import { Link } from 'react-router-dom';
 import './SearchBar.css';
 
 class SearchBar extends Component {
@@ -8,6 +10,9 @@ class SearchBar extends Component {
         <label className="search-bar-label" htmlFor="search-bar">
           Pesquisar
           <input className="search-bar-input" id="search-bar" type="text" />
+          <Link to="/Cart" data-testid="shopping-cart-button">
+            <img src={ cartImage } alt="Carrinho de compra" className="cart-img"/>
+          </Link>
         </label>
       </div>
     );
