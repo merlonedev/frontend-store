ffda3cd6ba212f34729b676f16ec27c462fd5232
Daniel <danielbped@gmail.com>
2021-07-12 19:47:28 -0300

    MovieDetails

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import SearchBar from './components/SearchBar';
 import SideBar from './components/SideBar';
 import Main from './pages/Main';
 import './App.css';
+import ItemDetails from './pages/ItemDetails';
 
 function App() {
   return (
     <BrowserRouter>
       <SearchBar />
       <Route exact path="/" component={ Main } />
+      <Route path="/details/:id" render={ (props) => <ItemDetails { ...props } /> } />
       <SideBar />
     </BrowserRouter>
   );
---
+++ src/pages/ItemDetails.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
+import * as api from '../services/api';
+
+class ItemDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      item: [],
+      loading: true,
+    };
+
+    this.getItem = this.getItem.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    this.getItem(id);
+  }
+
+  getItem(id) {
+    api.getProductsFromCategoryAndQuery(id, '$QUERY')
+      .then((response) => {
+        this.setState({
+          item: response.results.find((item) => item.id === id),
+          loading: false,
+        });
+      });
+  }
+
+  render() {
+    const { item, loading } = this.state;
+    const { title, price, thumbnail } = item;
+    return (
+      <main>
+        { loading && <Loading />}
+        <h1>{ title }</h1>
+        <img src={ thumbnail } alt={ title } />
+        <h2>
+          { `R$${price}` }
+        </h2>
+      </main>
+    );
+  }
+}
+
+ItemDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default ItemDetails;
