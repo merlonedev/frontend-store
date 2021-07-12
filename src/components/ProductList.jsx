db019faeaa664b81985d781c873c8cf9aaa86889
Josimar Souza <contact.josimarsouza@gmail.com>
2021-07-12 19:48:22 -0300

    Resolve problemas com lint

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ProductCard from './ProductCard';
-import { Link } from 'react-router-dom';
-
 
 export default class ProductList extends Component {
   render() {
     const { list } = this.props;
 
     return (
-      <section className="product-list">
-        { list.map((product) => <ProductCard key={ product.id } product={ product } />) }
+      <section>
+        <section className="product-list">
+          { list
+            .map((product) => <ProductCard key={ product.id } product={ product } />) }
+        </section>
+        <div>
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          <Link to="/shoppingcart">
+            <button data-testid="shopping-cart-button" type="button">Carrinho</button>
+          </Link>
+        </div>
       </section>
-      <div>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <Link to="/shoppingcart">
-          <button data-testid="shopping-cart-button" type="button">Carrinho</button>
-        </Link>
-      </div>
     );
   }
 }
