9bc6d2280a7c6c20491f9701c662d40431ed0dae
Dayvid Ruan <ruan12bbg@gmail.com>
2021-07-12 19:55:03 -0300

    criterio 5 finalizado

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import * as fetchApi from '../services/api';
 import SearchBar from './SearchBar';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      id: '',
+      product: [],
+    };
+
+    this.handlerChange = this.handlerChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handlerChange({ target }) {
+    this.setState({
+      search: target.value,
+    });
+  }
+
+  handleSubmit() {
+    const { search, id } = this.state;
+    fetchApi.getProductsFromCategoryAndQuery(search, id)
+      .then(({ results }) => (
+        this.setState({
+          product: results,
+        })
+      ));
+  }
+
   render() {
+    const { product } = this.state;
     return (
       <div>
-        <SearchBar />
+        <input
+          type="text"
+          data-testid="query-input"
+          placeholder="Buscar produto"
+          onChange={ this.handlerChange }
+        />
+
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleSubmit }
+        >
+          Clique Aqui
+        </button>
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <SearchBar product={ product } />
       </div>
     );
   }
