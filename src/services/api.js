41579efedd8a59ef4393918c333aedafc30fcfbc
julio11lp <julio11lp@hotmail.com>
2021-07-12 16:02:29 -0300

    Criterio 1 aprovado no lint e test sem comentarios e padronizado com try catch

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 export async function getCategories() {
-  // Implemente aqui
   const apiUrl = 'https://api.mercadolibre.com/sites/MLB/categories';
   try {
     const response = await fetch(apiUrl);
@@ -10,12 +9,13 @@ export async function getCategories() {
   }
 }
 
-getCategories();
-
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const apiUrl = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  const response = await fetch(apiUrl);
-  const categorie = await response.json();
-  return categorie;
+  try {
+    const response = await fetch(apiUrl);
+    const categorie = await response.json();
+    return categorie;
+  } catch (error) {
+    return error;
+  }
 }
