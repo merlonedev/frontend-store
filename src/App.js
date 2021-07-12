657f13f3c269bd1733e5dba1265e411b8aea2d42
Heder Waine <hederw@hotmail.com>
2021-07-12 19:23:57 -0300

    Requisito 04 - Resolve requisito 04

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Category from './components/Category';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ HomePage } />
+      <Category />
     </BrowserRouter>
   );
 }
