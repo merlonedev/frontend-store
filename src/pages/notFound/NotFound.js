d47e05853a3cdc2d791956ccc780cb8f01fccbcc
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-07-12 20:24:58 -0300

    Realiza correçao do lint - requisito 5

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -4,7 +4,7 @@ class NotFound extends Component {
   render() {
     return (
       <div>
-        <p>Nenhum produto foi encontrado</p>
+        <p>Nenhum produto foi encontrado!</p>
       </div>
     );
   }
