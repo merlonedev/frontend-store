6487fdc90b91072ff925e349e79c50998e570556
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-12 15:42:00 -0300

    altera app.js para classe e adiciona getCategories na api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(URL);
+  const categories = await response.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  
 }
