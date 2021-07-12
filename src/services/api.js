5c2ea104a0f2df80b4d88468639648db5f92379c
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-07-12 17:13:23 -0300

    implementa getCategories e corrige lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 export async function getCategories() {
-  const categories_api = "https://api.mercadolibre.com/sites/MLB/categories";
+  const categoriesApi = 'https://api.mercadolibre.com/sites/MLB/categories';
 
-  return fetch(categories_api)
+  return fetch(categoriesApi)
     .then((result) => result.json())
     .then((response) => response);
-  }
+}
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
