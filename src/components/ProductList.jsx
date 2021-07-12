8fbdc0cc120f37849a8d94ea03026ce7eee687e9
Patrick Dack <patrickdack89@gmail.com>
2021-07-12 15:48:26 -0300

    Requisito 2 - Implementado

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <h2
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+      </div>
+    );
+  }
+}
+
+export default ProductList;
