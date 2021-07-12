6d6d6d990b6f586e68b9028c3a2ff20b7f300600
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-07-12 16:32:15 -0300

    Requisito 01 - Inicia o projeto e resolve requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,19 @@
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    let result = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    result = await result.json();
+    return result;
+  } catch {
+    console.log('ERROR: could not fetch from api.mercadolibre');
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    let result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+    result = await result.json();
+    return result;
+  } catch {
+    console.log('ERROR: could not fetch from api.mercadolibre');
+  }
 }
