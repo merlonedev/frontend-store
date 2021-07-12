34a171a1dfe7ce3ec2f6728eaac3a51d0f63c776
Gabriel Viana <g.viana87@hotmail.com>
2021-07-12 18:42:35 -0300

    Add page Home/ Add BrowserRoute em App.js/ render ok

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     console.log(this.props);
   }
 
   render() {
-    return(
-      <p>oi</p>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
