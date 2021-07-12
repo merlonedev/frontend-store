dd4b7cf38527e6b4bcc65caf66555de4d00dbf45
rachidb5 <jordan.rachid@gmail.com>
2021-07-12 19:06:32 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
+import Cart from './pages/Cart';
 import SideBar from './components/SideBar';
 import Main from './pages/Main';
 import './App.css';
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <SearchBar />
       <Route exact path="/" component={ Main } />
+      <Route exact path="/Cart" component={ Cart } />
       <SideBar />
     </BrowserRouter>
   );
