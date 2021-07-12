6cc7432905454b9731be39eb7661cf0778b08cd1
Guilherme Dias <gmd.delta@gmail.com>
2021-07-12 17:24:42 -0300

    Resolvido requisito 2. Criado componentes ProductCard, ProductList e Search. Criado a Pagina ProductsPage.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-// teste PR
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import ProductsPage from './pages/ProductsPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route to="/" component={ ProductsPage } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
