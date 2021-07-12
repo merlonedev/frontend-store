fdc089b3eb145cce785719f13859b46208609fb8
Jossany Moura <mjossany@gmail.com>
2021-07-12 19:15:14 -0300

    Finaliza requisito 4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import SearchBar from './Components/SearchBar';
+import FilterCategories from './Components/FilterCategories';
+
 
 function App() {
   return (
     <div>
       <SearchBar />
+      <FilterCategories />
     </div>
   );
 }
