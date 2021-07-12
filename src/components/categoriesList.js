53ed98f9e217bbf92c75d0a47f8fe2b9d923945e
matheustkaczyk <matheus111904@hotmail.com>
2021-07-12 18:36:49 -0300

    Chamando a funçao getCategories no componente e setando o estado de cada categoria

--- src/components/categoriesList.js
+++ src/components/categoriesList.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Categories extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: {},
+    };
+
+    this.renderCategories = this.renderCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.renderCategories();
+  }
+
+  async renderCategories() {
+    const datas = await getCategories();
+    const categoriesName = datas.map((data) => data.name);
+    this.setState({ categories: categoriesName });
+  }
+
   render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
       <p>teste</p>
     );
