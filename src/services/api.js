fc73962a5a4474d6674e36e53bd5fb798ec56c4d
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-07-12 16:36:46 -0300

    Requisito 1 - Implementa funçao getCategories()

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,10 @@
 export async function getCategories() {
-  // Implemente aqui
+  const categories = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/categories',
+  )
+    .then((response) => response.json())
+    .then((data) => data);
+  return categories;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
