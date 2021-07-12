103ff94856a06e540522307d0835e092e9b47c4f
David Azevedo <zevid.dev@outlook.com>
2021-07-12 15:29:52 -0300

    Feito requisito-2. Cria Componente Index.js, com Campo Input e instruçao para pesquisa de produto e renderiza em App.js

---
+++ src/components/Index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Index extends React.Component {
+  render() {
+    return (
+      <>
+        <input
+          type="text"
+        />
+        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </>
+    );
+  }
+}
+
+export default Index;
