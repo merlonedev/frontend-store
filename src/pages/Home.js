884658dd1ed93422ff09612cd7e6722d4b0efee2
Bernardo Coelho <bernardocezario21@gmail.com>
2021-07-12 17:27:40 -0300

    Requisito 5 Completo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import * as api from '../services/api';
+import Products from './Products';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+      categoryId: '',
+      productList: [],
+    };
+    this.inputList = this.inputList.bind(this);
+    this.requestProducts = this.requestProducts.bind(this);
+  }
+
+  inputList({ target }) {
+    this.setState({
+      search: target.value,
+    });
+  }
+
+  requestProducts() {
+    const { categoryId, search } = this.state;
+    api.getProductsFromCategoryAndQuery(categoryId, search)
+      .then(({ results }) => (
+        this.setState({
+          productList: results,
+        })
+      ));
+  }
+
   render() {
+    const { productList } = this.state;
     return (
       <form>
         <p
@@ -9,8 +39,22 @@ class Home extends React.Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <input
+          type="text"
+          data-testid="query-input"
+          onChange={ this.inputList }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.requestProducts }
+        >
+          Clique Aqui
+        </button>
+        <Products productList={ productList } />
       </form>
     );
   }
 }
+
 export default Home;
