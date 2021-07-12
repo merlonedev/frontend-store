22f854989b8f80ca05558cf39756a132740b7e57
Luís Seixas <luiseixas2@gmail.com>
2021-07-12 19:07:57 -0300

    importando o css no App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
 import ShoppingCart from './pages/ShoppingCart';
+import './App.css';
 
 function App() {
   return (
