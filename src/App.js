2655167cd786cace52823911fd94db9885f39a15
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-12 15:55:07 -0300

    adiciona getProductsFromCategoryAndQuery na api.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import * as api from './services/api';
 
 class App extends React.Component {
   render() {
+    api.getProductsFromCategoryAndQuery("MLB5672", null).then((r) => console.log(r.results));
     return (<p></p>);
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -6,5 +6,20 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  
+  if (categoryId && query) {
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    const response = await fetch(URL);
+    const results = await response.json();
+    return results;
+  } else if (query) {
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+    const response = await fetch(URL);
+    const results = await response.json();
+    return results;
+  } else {
+    const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+    const response = await fetch(URL);
+    const results = await response.json();
+    return results;
+  }
 }
