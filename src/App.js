946fbe6a70cf96e5db888c186b689fddd73a1b64
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-07-12 16:17:35 -0300

    Requisito 2 resolvido

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main className="">
+        <header>
+          <label htmlFor="searchText" data-testid="home-initial-message" className="labelSeach">
+            <input type="text" name="seachText" className="searchInput" />
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </label>
+        </header>
+      </main>
+    );
+  }
 }
 
 export default App;
