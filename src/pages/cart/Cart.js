b362bd9c927a784fa8f3ee97d7fbe1f2a804ff41
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-07-13 18:29:54 -0300

    Realiza requisito 8 - adiciona itens ao carrinho e monta carrinho no local storage

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { AiFillHome } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import CartCard from '../../components/cartcard/CartCard';
 
 class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartItems: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.cartSetState();
+  }
+
+  cartSetState = () => {
+    let local = localStorage.getItem('Cart');
+    local = JSON.parse(local);
+    this.setState({
+      cartItems: [...local],
+    });
+  }
+
   render() {
+    const { cartItems } = this.state;
+    if (!cartItems) {
+      return <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>;
+    }
     return (
-      <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+      <section>
+        <Link to="/"><AiFillHome color="green" /></Link>
+        {cartItems.map((item) => <CartCard key={ item.id } product={ item } />)}
+      </section>
+
     );
   }
 }
