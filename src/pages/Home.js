5add364341bf9bf172beee101cc55a47c2d22909
Thalles Carneiro <thallescarneiro@gmail.com>
2021-07-13 16:46:42 -0300

    [Group 31] - Requisito 6 - Implementa filtro de produtos de acordo com a categoria selecionada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ class Home extends React.Component {
 
     this.setState({
       [name]: value,
-    });
+    }, () => this.getProductList());
   }
 
   async getProductList() {
@@ -70,7 +70,7 @@ class Home extends React.Component {
         <main className="main-content-container">
           { (buttonClick) ? <ProductList productList={ productList } /> : initialMsg }
         </main>
-        <Categories />
+        <Categories handleChange={ this.handleChange } />
       </div>
     );
   }
