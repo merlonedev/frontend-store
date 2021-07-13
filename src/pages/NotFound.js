abaab27018a45f63083c147a8e532223233c4c4a
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-13 17:15:42 -0300

    Cria NotFound e corrige rotas

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <main>
+        <p>Página não encontrada</p>
+      </main>
+    );
+  }
+}
+
+export default NotFound;
