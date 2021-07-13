72401d72168dd7f051ce232eb2395ad3c2d8cd86
martinsph <ph.mmartins@gmail.com>
2021-07-13 16:45:54 -0300

    corrige lint req 12.5

--- src/components/CheckoutButton.jsx
+++ src/components/CheckoutButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Link from 'react-router-dom';
 
 export default class CheckoutButton extends React.Component {
-  render () {
-    <button type="button">Finalizar Compra</button>
-    </Link>
-    <Link to="/checkout" >
+  render() {
+    return (
+      <Link to="/checkout">
+        <button type="button">Finalizar Compra</button>
+      </Link>
+    );
   }
 }
