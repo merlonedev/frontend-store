87784fcb3bb04183175c6be49c91a30f3fa6251a
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-13 13:39:24 -0400

    captura o input da pesquisa e joga para o component serchResult

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,45 @@ import List from '../Components/List';
 import SearchResult from '../Components/SearchResult';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      input: '',
+      textToSearch: '',
+    };
+
+    this.getSearch = this.getSearch.bind(this);
+    this.doSearch = this.doSearch.bind(this);
+  }
+
+  getSearch(change) {
+    this.setState({
+      input: change,
+    });
+  }
+
+  doSearch() {
+    const { input } = this.state;
+    this.setState({
+      textToSearch: input,
+    });
+  }
+
   render() {
+    const { textToSearch } = this.state;
     return (
       <div>
-        <List />
-        <SearchResult />
+        <List textChange={ this.getSearch } />
+        <button
+          type="button"
+          onClick={ this.doSearch }
+          data-testid="query-button"
+        >
+          Pesquisar
+        </button>
+        {/* {clickSearch && <SearchResult textToSearch={ search } />} */}
+        <SearchResult textToSearch={ textToSearch } />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
