95f3335312ff363b410a16a5c7b18e0fea50d424
Eurides <euridesalves@yahoo.com.br>
2021-07-13 20:11:43 -0300

    Corrigido css requisito 3

--- src/components/ShoppingCardIcon.js
+++ src/components/ShoppingCardIcon.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class ShoppingCardIcon extends Component {
   render() {
     return (
-      <img className="App-logo" src="https://image.flaticon.com/icons/png/512/5008/5008618.png" alt="imagem de carrinho de compras" />
+      <img className="App-logo-new" src="https://image.flaticon.com/icons/png/512/5008/5008618.png" alt="imagem de carrinho de compras" />
     );
   }
 }
