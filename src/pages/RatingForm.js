cfc144bd3e594c8997b7144e9d98314290af32a4
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-07-13 14:28:55 -0300

    refatorando codigo

--- src/pages/RatingForm.js
+++ src/pages/RatingForm.js
@@ -6,44 +6,20 @@ class RatingForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      quantity: 0,
       email: '',
     };
-    this.handleQuantity = this.handleQuantity.bind(this);
     this.handleEmailValue = this.handleEmailValue.bind(this);
-  }
-
-  handleQuantity(e, operator) {
-    e.preventDefault();
-    if (operator === "+") {
-      this.setState((prevState) => ({ quantity: prevState.quantity + 1 }));
-    } else {
-      this.setState((prevState) => (
-        prevState.quantity > 0 ? ({ quantity: prevState.quantity - 1 }) : ({ quantity: prevState.quantity })));
-    }
   }
 
   handleEmailValue({ target: { value } }) {
     this.setState({ email: value })
   }
   render() {
-    const { quantity, email } = this.state;
-    const { handleQuantity, handleEmailValue } = this;
+    const {  email } = this.state;
+    const { handleEmailValue } = this;
     
     return (
       <section>
-        <div>
-          <h2>Produto</h2>
-          <img></img>
-          <ul></ul>
-        </div>
-        <div>
-          <h2>Quantidade</h2>
-          <Button type="button" title="+" onClick={ (e) => handleQuantity(e, '+') } />
-          <span>{ quantity }</span>
-          <Button type="button" title="-" onClick={ (e) => handleQuantity(e, '-') } />
-          <Button type="button" title="Adicionar ao Carrinho" onClick={ () => console.log('adicionado') }/>
-        </div>
         <div>
           <h2>Avaliações</h2>
           <form>
