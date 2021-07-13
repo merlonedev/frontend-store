0449f2a784684884a4fb714bf7b82c194fe969b5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-07-13 09:09:27 -0300

    requisito 10 - manipulacao de quantidade

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+// import { findIndex } from '../__mocks__/categories';
 
 export default class CartItems extends Component {
   constructor() {
@@ -9,6 +10,8 @@ export default class CartItems extends Component {
     };
 
     this.getProductFromId = this.getProductFromId.bind(this);
+    this.increaseQty = this.increaseQty.bind(this);
+    this.decreaseQty = this.decreaseQty.bind(this);
   }
 
   componentDidMount() {
@@ -20,6 +23,7 @@ export default class CartItems extends Component {
     const apiURL = `https://api.mercadolibre.com/items/${id}`;
     const resultRequest = await fetch(apiURL);
     const result = await resultRequest.json();
+    result.qty = 1;
     this.setState((prevState) => {
       const previous = [...prevState.items];
       previous.push(result);
@@ -27,13 +31,54 @@ export default class CartItems extends Component {
     });
   }
 
+  increaseQty({ target }) { // credits: https://stackoverflow.com/questions/37662708/react-updating-state-when-state-is-an-array-of-objects
+    const { items } = this.state;
+    const itemToIncrease = target.parentElement.id;
+    const itemIndex = items.findIndex(({ id }) => id === itemToIncrease);
+    this.setState({
+      items: [
+        ...items.slice(0, itemIndex),
+        { ...items[itemIndex], qty: items[itemIndex].qty + 1 },
+        ...items.slice(itemIndex + 1),
+      ],
+    });
+  }
+
+  decreaseQty({ target }) {
+    const { items } = this.state;
+    const itemToDecrease = target.parentElement.id;
+    const itemIndex = items.findIndex(({ id }) => id === itemToDecrease);
+    if (items[itemIndex].qty < 2) return;
+    this.setState({
+      items: [
+        ...items.slice(0, itemIndex),
+        { ...items[itemIndex], qty: items[itemIndex].qty - 1 },
+        ...items.slice(itemIndex + 1),
+      ],
+    });
+  }
+
   render() {
     const { items } = this.state;
     return items.map((item) => (
-      <div key={ item.id }>
+      <div key={ item.id } id={ item.id }>
         <p data-testid="shopping-cart-product-name">{item.title}</p>
-        <p>{item.price}</p>
-        <p data-testid="shopping-cart-product-quantity">1</p>
+        <p>{item.price * item.qty}</p>
+        <p data-testid="shopping-cart-product-quantity">{item.qty}</p>
+        <button
+          data-testid="product-increase-quantity"
+          type="button"
+          onClick={ this.increaseQty }
+        >
+          +
+        </button>
+        <button
+          data-testid="product-decrease-quantity"
+          type="button"
+          onClick={ this.decreaseQty }
+        >
+          -
+        </button>
       </div>
     ));
   }
