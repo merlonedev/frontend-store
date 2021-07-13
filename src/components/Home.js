aee642bfff9314cf6227abd17c2e15f1580d23c4
thales-daniel <thalesdaniel2013@gmail.com>
2021-07-12 23:13:41 -0300

    feito de verdade agora

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,9 @@ import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
 
 class Home extends Component {
-
   render() {
     return (
       <div>
-    
         <SearchBar />
         <header>
           <Link to="/ShoppingCart" data-testid="shopping-cart-button">
