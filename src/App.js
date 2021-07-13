380c57a1963c78ec861b715a77c2d1247b642f27
Kevin Oliveira <kevin.zero@hotmail.com>
2021-07-13 16:50:13 -0300

    Cria pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
-import ProductList from './components/ProductList';
+import Checkout from './pages/Checkout';
+import Details from './pages/Details';
+import Main from './pages/Main';
+import ShoppingCart from './pages/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route
-            to="/"
-            component={ ProductList }
-          />
+          <Route to="/checkout" component={ Checkout } />
+          <Route to="/details" component={ Details } />
+          <Route to="/cart" component={ ShoppingCart } />
+          <Route exact to="/" component={ Main } />
         </Switch>
       </BrowserRouter>
     );
