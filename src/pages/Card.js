20c5b451be78f039022d441e4d8350de8ae3c136
Ramon Paschoal <rpaschoal90@gmail.com>
2021-07-14 10:49:25 -0300

    Requisito 7 - refatorado

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,45 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Card extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
-    
+    this.setProduct = this.setProduct.bind(this);
+
+    this.state = {
+      product: {},
+    };
   }
 
-  render() {
+  componentDidMount() {
+    this.setProduct();
+  }
+
+  async setProduct() {
     const { history } = this.props;
     const { location } = history;
     const { state } = location;
-    const { id, title, thumbnail, price, address, seller_contact } = state;
-    const { state_name } = address;
-    const { contact } = seller_contact;
+    const { title, id } = state;
+    const { results } = await getProductsFromCategoryAndQuery('', title);
+    const product = results.find((result) => result.id === id);
+    this.setState({ product });
+  }
 
-    console.log(state)
+  render() {
+    const { product } = this.state;
+    const { title, thumbnail, price, condition } = product;
+    // const { state_name: stateName } = address;
 
+    // console.log(this.props);
     return (
-      <div>
-        <h1 data-testeid="product-detail-name">{ title }</h1>
-        <img src={ thumbnail } alt="Product" />
-        <div className="infos">
-          <ul>
-            Especificações Técnicas
-            <li>{ title }</li>
-            <li>Preço: R$ { price }</li>
-            <li>{ state_name }</li>
-            <li>{ contact }</li>
-          </ul>
-        </div>
+      <div data-testeid="">
+        { !title ? <p>Loading...</p>
+          : (
+            <div>
+              <h1 data-testeid="product-detail-name">{ title }</h1>
+              <img src={ thumbnail } alt="Product" />
+              <div className="infos">
+                <ul>
+                  Especificações Técnicas
+                  <li>{ title }</li>
+                  <li>
+                    Preço: R$
+                    { price }
+                  </li>
+                  {/* <li>{ stateName }</li> */}
+                  <li>
+                    Condição:
+                    { condition }
+                  </li>
+                </ul>
+              </div>
+            </div>)}
       </div>
     );
   }
 }
 
 Card.propTypes = {
-  title: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  thumbnail: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    location: PropTypes.shape({
+      state: PropTypes.shape({
+        title: PropTypes.string,
+        thumbnail: PropTypes.string,
+        id: PropTypes.string,
+        address: PropTypes.shape({
+          state_name: PropTypes.string,
+        }),
+        price: PropTypes.number,
+        condition: PropTypes.string,
+      }),
+    }),
+  }).isRequired,
 };
 
 export default Card;
