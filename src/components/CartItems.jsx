cde69b0701e78149a317ca00c55eed930724b8bb
alestori <alestori7@gmail.com>
2021-07-14 17:21:55 -0300

    correção de falha em testes após merge de branches conflitantes

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -25,9 +25,9 @@ export default class CartItems extends Component {
 
   removeItem({ target }) {
     const itemToRemove = target.parentElement.id;
-    const qtyToRemove = Number(target.parentElement.className);
+    // const qtyToRemove = Number(target.parentElement.className);
     const { handlers } = this.props;
-    handlers.remove(itemToRemove, qtyToRemove);
+    handlers.remove(itemToRemove);
   }
 
   renderButtons(id, qty) {
