c0109a93d7a2dada4d59fb8ae4e60038c9abc758
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-14 13:46:12 -0400

    remove importaçao de productCard

--- src/Components/SearchResult.js
+++ src/Components/SearchResult.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import ProductCard from './ProductCard';
-
 class SearchResult extends React.Component {
   constructor(props) {
     super(props);
