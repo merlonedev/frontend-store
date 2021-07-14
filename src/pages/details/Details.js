0eb35f49123e40d832e5b97db342ae5b4f1563cb
netosilv4 <netosilv44445@gmail.com>
2021-07-14 18:43:33 -0300

    Requisito 11

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import AvaliationForm from '../../components/avaliationForm/AvaliationForm';
+import Feedbacks from '../../components/feedbacks/Feedbacks';
 
 class Details extends Component {
   constructor() {
     super();
     this.localStorageGet = this.localStorageGet.bind(this);
+    this.formClickHandler = this.formClickHandler.bind(this);
+    this.formHandleChange = this.formHandleChange.bind(this);
+    this.saveFeedBacks = this.saveFeedBacks.bind(this);
+    this.localFeedbacks = this.localFeedbacks.bind(this);
     this.state = {
       product: {},
+      userComent: '',
+      userRating: '',
+      userEmail: '',
+      usersFeedbacks: [],
     };
   }
 
@@ -15,23 +25,68 @@ class Details extends Component {
 
   localStorageGet() {
     const productStorage = JSON.parse(localStorage.getItem('prdctDetails'));
-    console.log(productStorage);
     this.setState({
       product: productStorage,
+    }, () => this.localFeedbacks());
+  }
+
+  localFeedbacks() {
+    const { product } = this.state;
+    const feedbacks = JSON.parse(localStorage.getItem(`usersFeedbacks${product.id}`));
+    this.setState({
+      usersFeedbacks: feedbacks !== null ? [...feedbacks] : [],
     });
   }
 
-  render() {
+  formHandleChange({ target }) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
+  formClickHandler() {
+    const { userComent, userRating, userEmail } = this.state;
+    const feedback = {
+      userComent: '',
+      userRating: '',
+      userEmail: '',
+    };
+    feedback.userComent = userComent;
+    feedback.userRating = userRating;
+    feedback.userEmail = userEmail;
+    this.setState((e) => ({
+      usersFeedbacks: [...e.usersFeedbacks, feedback],
+    }), () => this.saveFeedBacks());
+  }
+
+  saveFeedBacks() {
     const { product } = this.state;
+    const { usersFeedbacks } = this.state;
+    localStorage.setItem(`usersFeedbacks${product.id}`, JSON.stringify(usersFeedbacks));
+  }
+
+  render() {
+    const { product, userComent, userRating, usersFeedbacks, userEmail } = this.state;
     const { title, price, thumbnail, sold_quantity: soldQt, condition,
       available_quantity: avaliables } = product;
     return (
-      <div>
-        <img alt="Product Cover" src={ thumbnail } />
-        <p>{ `${condition} | Preço: R$ ${price}` }</p>
-        <p data-testid="product-detail-name">{ `Nome: ${title}` }</p>
-        <p>{ `Quantidade vendida: ${soldQt}( ${avaliables} disponíveis)` }</p>
-      </div>
+      <section>
+        <div>
+          <img alt="Product Cover" src={ thumbnail } />
+          <p>{ `${condition} | Preço: R$ ${price}` }</p>
+          <p data-testid="product-detail-name">{ `Nome: ${title}` }</p>
+          <p>{ `Quantidade vendida: ${soldQt}( ${avaliables} disponíveis)` }</p>
+        </div>
+        <AvaliationForm
+          userComent={ userComent }
+          userRating={ userRating }
+          formClickHandler={ this.formClickHandler }
+          formHandleChange={ this.formHandleChange }
+          userEmail={ userEmail }
+        />
+        <Feedbacks usersFeedbacks={ usersFeedbacks } />
+      </section>
+
     );
   }
 }
