b7e16f8fdce853ae71ccee39f5d4b3494ca0ea9b
David Azevedo <zevid.dev@outlook.com>
2021-07-14 15:23:14 -0300

    Cria funcao countItemCart, em App. Para contabilizar os itens no carrinho

--- src/App.js
+++ src/App.js
@@ -11,14 +11,17 @@ class App extends React.Component {
     super();
     this.state = {
       itemCart: [],
+      count: 0,
     };
     this.addToCartItem = this.addToCartItem.bind(this);
+    this.countItemCart = this.countItemCart.bind(this);
   }
 
   // Consegui esse requisito com ajuda do Inácio da turma 11
   // Essa função vai adicionar o item no state itemCart, verificando se ele existe repetido ou não
   // Caso ele seja repetido ele vai apenas aumentar a quantidade do produto.
   addToCartItem(item) {
+    this.countItemCart();
     this.setState(({ itemCart }) => {
       const index = itemCart.findIndex(({ id }) => id === item.id);
       item.quantity = 1;
@@ -31,14 +34,30 @@ class App extends React.Component {
     });
   }
 
+  countItemCart() {
+    this.setState((state) => ({ count: state.count + 1 }));
+  }
+
   render() {
-    const { itemCart } = this.state;
+    const { itemCart, count } = this.state;
     return (
       <BrowserRouter>
         <Switch>
-          <Route path="/infos/:id/:product" component={ Infos } />
+          <Route
+            path="/infos/:id/:product"
+            render={ (props) => (<Infos
+              { ...props }
+              count={ count }
+            />) }
+          />
           <Route path="/cart" render={ () => <Cart itemCart={ itemCart } /> } />
-          <Route path="/" render={ () => <Index addCartItem={ this.addToCartItem } /> } />
+          <Route
+            path="/"
+            render={ () => (<Index
+              addCartItem={ this.addToCartItem }
+              count={ count }
+            />) }
+          />
         </Switch>
       </BrowserRouter>
     );
