c7d32a9131a0968be0c836be79cdb874cb2c54e4
Kevin Willian <kevinwillian084@gmail.com>
2021-07-14 01:41:45 -0300

    Refatora disposicao dos components: Requisito 1-9

--- src/components/ShoppingCart/CartButton.jsx
+++ src/components/ShoppingCart/CartButton.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+// Comentario Apenas para commitar;
 class CartButton extends Component {
   render() {
     return (
