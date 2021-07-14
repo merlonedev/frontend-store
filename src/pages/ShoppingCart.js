e8622173c33bdf0a07acbf96fc4b5066526be303
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-13 22:15:33 -0400

    renderiza os produtos no carrinho

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ShoppingCartIcon from '../Components/ShoppingCartIcon';
 
 class ShoppingCart extends React.Component {
   render() {
+    const { products } = this.props;
     return (
       <div>
         <ShoppingCartIcon />
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {
+          products.length === 0
+          && <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        }
+        {
+          products.map(({ id, title, thumbnail }) => (
+            <div key={ id }>
+              <h3 data-testid="shopping-cart-product-name">{title}</h3>
+              <img src={ thumbnail } alt="Imagem do produto" />
+            </div>
+          ))
+        }
       </div>
     );
   }
 }
+
+ShoppingCart.propTypes = {
+  products: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+};
 
 export default ShoppingCart;
