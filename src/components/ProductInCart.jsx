9586b59455d3b5faed35226dcc3bf67f1fe22cf0
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-07-13 23:25:07 -0300

    adiciona botões para remoção de item do carrinho e para aumentar e diminuir quantidade de item alem de estilizar itens do carrinho

--- src/components/ProductInCart.jsx
+++ src/components/ProductInCart.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import {
+  AiFillCloseCircle,
+  AiFillPlusCircle,
+  AiFillMinusCircle,
+} from 'react-icons/ai';
+import '../css/productInCart.css';
 import PropTypes from 'prop-types';
 
 class ProductInCart extends React.Component {
   render() {
     const { product: { title, thumbnail, price } } = this.props;
     return (
-      <div data-testid="product">
-        <p data-testid="shopping-cart-product-name">{title}</p>
-        <img src={ thumbnail.replace('I.jpg', 'O.jpg') } alt={ title } />
+      <div data-testid="product" className="product">
+        <AiFillCloseCircle className="remove-item-cart" />
+        <img
+          src={ thumbnail.replace('I.jpg', 'O.jpg') }
+          alt={ title }
+          className="image-item"
+        />
+        <p className="item-cart-name" data-testid="shopping-cart-product-name">{title}</p>
+        <AiFillMinusCircle className="minus-item" />
+        <div className="item-price-content">
+          <span data-testid="shopping-cart-product-quantity">1</span>
+        </div>
+        <AiFillPlusCircle className="plus-item" />
         <p>{price}</p>
-        <div data-testid="shopping-cart-product-quantity">1</div>
       </div>
     );
   }
