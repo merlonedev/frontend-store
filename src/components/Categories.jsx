d852c1dc7371336bbff0340d104fe6e5afcf8fff
paulomichael <paulo.michael@gmail.com>
2021-07-13 22:59:25 -0300

    funcoes requisicoes na api

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,29 +1,22 @@
-// import React, { Component } from 'react';
-// import * as Api from './services/api'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// class Categories extends Component {
-//   constructor() {
-//     super();
-//     this.state= {
-//       categories: [],
-//     }
+class Categories extends Component {
+  render() {
+    const { category } = this.props;
+    const { name } = category;
+    return (
+      <div>
+        <h4>{name}</h4>
+      </div>
+    );
+  }
+}
 
-//   }
+Categories.propTypes = {
+  category: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
-//   componentDidMount() {
-//     Api.getCategories().then((response) => this.setState({
-//       categories: response,
-//     }))
-//   }
-
-//   render() {
-//     const { categories } = this.state;
-//     return(
-//       <div>
-//         {categories}
-//       </div>
-//     )
-//   }
-// }
-
-// export default Categories;
+export default Categories;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar';
+import * as api from '../services/api';
+import Categories from '../components/Categories';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      status: true,
+      text: '',
+      data: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.apiRequest = this.apiRequest.bind(this);
+  }
+
+  componentDidMount() {}
+
+  handleChange({ target }) {
+    const { text } = this.state;
+    const { value } = target;
+    this.setState({ text: value });
+  }
+
+  async apiRequest(id, query) {
+    const { categories, data } = this.state;
+    const allCategories = await api.getCategories();
+    const searchProduct = await api.getProductsFromCategoryAndQuery(id, query);
+    this.setState({
+      data: searchProduct.results,
+      categories: allCategories,
+    });
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div>
         <SearchBar />
+        { data.map((categories) => (<Categories
+          key={ categories.id }
+          category={ categories }
+        />))}
       </div>
     );
   }
