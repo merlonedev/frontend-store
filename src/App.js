b64077dd1d4ad19318cc1aeeac4a84528f1b2fd2
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-07-14 18:36:15 -0300

    requisito 7 - cria a pagina de detalhe do produto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Carrinho from './components/Carrinho';
+import ProductDetail from './pages/ProductDetail';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
-          <Route exact path="/carrinho" component={ Carrinho } />
+          <Route path="/carrinho" component={ Carrinho } />
+          <Route path="/productDetail/:id" component={ ProductDetail } />
         </Switch>
       </BrowserRouter>
     </div>
--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 const URL = 'https://api.mercadolibre.com/sites/MLB/';
+const ADJUST = -10;
 
 export async function getCategories() {
   return fetch(`${URL}categories`)
@@ -6,6 +7,10 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId === 'MLB') {
+    return fetch(`${URL.slice(0, ADJUST)}items?ids=${query}`)
+      .then((response) => response.json());
+  }
   return fetch(`${URL}search?category=${categoryId}&q=${query}`)
     .then((response) => response.json());
 }
