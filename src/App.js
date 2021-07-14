2cf3f7e85a09644ea946c8313d0d3f531fd3ea93
Bruno Carvalho Trindade <bruno.fox_carvalho@hotmail.com>
2021-07-13 21:05:49 -0300

    Pre-finalizaçao do ProductDetails

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Components/Home';
 import Cart from './Components/Cart';
 import './App.css';
 import NavBar from './Components/NavBar';
+import ProductDetails from './Components/ProductDetails';
 
 class App extends React.Component {
   render() {
@@ -13,7 +14,12 @@ class App extends React.Component {
           <BrowserRouter>
             <Switch>
               <Route path="/Cart" component={ Cart } />
+              <Route
+                path="/product-detail/:categoryId/:id"
+                render={ (props) => <ProductDetails { ...props } /> }
+              />
               <Route path="/" component={ Home } exact />
+              <Route path="/ProductDetails" component={ ProductDetails } />
             </Switch>
           </BrowserRouter>
           <NavBar />
--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as api from '../services/api';
 
 class ProductDetails extends Component {
   constructor() {
     super();
-    this.state={
+    this.state = {
       product: [],
     };
     this.getProducts = this.getProducts.bind(this);
@@ -16,8 +17,8 @@ class ProductDetails extends Component {
   }
 
   async getProducts() {
-    const { match: { params: { categoryId, '' } } } = this.props;
-    const requisitionApi = await api.getProductsFromCategoryAndQuery(categoryId, id);
+    const { match: { params: { categoryId, id } } } = this.props;
+    const requisitionApi = await api.getProductsFromCategoryAndQuery(categoryId, '');
     const details = requisitionApi.results.find((product) => product.id === id);
 
     this.setState({
@@ -26,23 +27,35 @@ class ProductDetails extends Component {
   }
 
   render() {
+    const { product: { title, thumbnail, price } } = this.state;
     return (
       <section>
-        <h3 />
-        <button />
+        <Link to="/">
+          Voltar
+        </Link>
+        <Link to="/Components/Cart">
+          Carrinho
+        </Link>
+        <h3 data-testid="product-detail-name">
+          { title }
+          -
+          { price }
+        </h3>
         <div>
-          <img> {/* imagem do carrinho */} </img>
-        </div>
-        <h5 />
-        <lu>
-          {/* <li></li> */}
-        </lu>
-        <div>
-
+          <img alt="imagem do produto" src={ thumbnail } />
         </div>
       </section>
     );
   }
 }
 
-export default ProductDetails
+export default ProductDetails;
+
+ProductDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+      categoryId: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
