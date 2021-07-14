50fbf9788415e1e4a58dfac60a674bbac9a767ab
lucasnahas <lucasahas@gmail.com>
2021-07-14 20:12:50 -0300

    Requisito 7 feito

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Details extends Component {
   render() {
+    const { props: { location: { state } } } = this;
+    const { title, price, thumbnail } = state;
     return (
-      <main>
-        <p>{this.props.match.params.id}</p>
-      </main>
+      <div>
+        <img alt="imagem do produto" src={ thumbnail } />
+        <h3 data-testid="product-detail-name">{ title }</h3>
+        <p>{`R$ ${price}`}</p>
+      </div>
     );
   }
 }
+
+Details.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      title: PropTypes.string,
+      price: PropTypes.number,
+      thumbnail: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Details;
