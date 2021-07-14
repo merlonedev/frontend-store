ec90e90d78e5548e0f19363ad195fc54c979bd55
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-14 16:58:49 -0300

    Requisito 8: Correçao

--- src/Pages/CartPages.js
+++ src/Pages/CartPages.js
@@ -1,13 +1,57 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CartItemProduct from '../Components/CartItemProduct';
 
 class CartPages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [
+        ...this.getItens(),
+      ],
+    };
+
+    this.getItens = this.getItens.bind(this);
+    this.renderCartItens = this.renderCartItens.bind(this);
+  }
+
+  getItens() {
+    const keys = Object.keys(localStorage);
+    return keys.map((key) => JSON.parse(localStorage.getItem(key)));
+  }
+
+  renderCartItens() {
+    const { products } = this.state;
+    return (
+      products.length > 0
+        ? products.map((product) => (
+          <CartItemProduct
+            key={ product.id }
+            product={ product }
+          />
+        ))
+        : <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>
+    );
+  }
+
   render() {
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        { this.renderCartItens() }
       </div>
     );
   }
 }
+
+CartPages.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+  }),
+};
+
+CartPages.defaultProps = {
+  product: PropTypes.object,
+};
 
 export default CartPages;
