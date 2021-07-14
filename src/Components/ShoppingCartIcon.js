54dc7deac690c2b94d959382b18cb1fe34085513
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-07-14 19:24:46 -0300

    Grupo 29 - Completa requisito 10

--- src/Components/ShoppingCartIcon.js
+++ src/Components/ShoppingCartIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 class ShoppingCartIcon extends React.Component {
   render() {
     return (
-      <img src="https://image.flaticon.com/icons/png/512/263/263142.png" alt="ícone de carrinho de compras" />
+      <img src="https://image.flaticon.com/icons/png/512/263/263142.png" alt="ícone de carrinho de compras" className="shopping-cart-size" />
     );
   }
 }
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -4,25 +4,77 @@ import ShoppingCartIcon from '../Components/ShoppingCartIcon';
 
 class ShoppingCart extends React.Component {
   render() {
-    const { products } = this.props;
+    const { products, increaseQuantity, decreaseQuantity, removeThisItem } = this.props;
+    const total = products.reduce((acc, cur) => acc + cur.price * cur.quantityInCart, 0);
     return (
       <div>
-        <ShoppingCartIcon />
-        {
-          products.length === 0
-          && <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-        }
-        {
-          products.map(({ id, title, thumbnail, quantityInCart }) => (
-            <div key={ id }>
-              <h3 data-testid="shopping-cart-product-name">{title}</h3>
-              <img src={ thumbnail } alt="Imagem do produto" />
-              <p data-testid="shopping-cart-product-quantity">
-                {`Quantidade: ${quantityInCart}`}
-              </p>
-            </div>
-          ))
-        }
+        <header className="row">
+          <ShoppingCartIcon />
+          <h2>Carrinho de compras</h2>
+        </header>
+        <main>
+
+          <div>
+
+            {
+              products.length === 0
+            && <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+            }
+            {
+              products.map((item) => {
+                const { id, title, thumbnail, quantityInCart, price } = item;
+                return (
+                  <div key={ id } className="row">
+                    <button
+                      type="button"
+                      className="remove-item"
+                      onClick={ () => removeThisItem(item) }
+                    >
+                      x
+                    </button>
+                    <img src={ thumbnail } alt={ title } className="thumb-size" />
+                    <h3 data-testid="shopping-cart-product-name">{title}</h3>
+                    <button
+                      type="button"
+                      data-testid="product-decrease-quantity"
+                      onClick={ () => decreaseQuantity(item) }
+                      className="remove"
+                    >
+                      -
+                    </button>
+                    <button
+                      type="button"
+                      data-testid="shopping-cart-product-quantity"
+                      className="badge"
+                    >
+                      {quantityInCart}
+                    </button>
+                    <button
+                      type="button"
+                      data-testid="product-increase-quantity"
+                      onClick={ () => increaseQuantity(item) }
+                      className="add"
+                    >
+                      +
+                    </button>
+                    <p>
+                      R$
+                      { quantityInCart * price.toFixed(2) }
+                    </p>
+                  </div>
+                );
+              })
+            }
+            {
+              products.length !== 0
+              && <h4>{`Valor Total da Compra: R$${total.toFixed(2)}`}</h4>
+            }
+
+            {
+              products.length !== 0 && <button type="button">Finalizar Compra</button>
+            }
+          </div>
+        </main>
       </div>
     );
   }
@@ -34,7 +86,11 @@ ShoppingCart.propTypes = {
     title: PropTypes.string.isRequired,
     thumbnail: PropTypes.string.isRequired,
     quantityInCart: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
   }).isRequired).isRequired,
+  increaseQuantity: PropTypes.func.isRequired,
+  decreaseQuantity: PropTypes.func.isRequired,
+  removeThisItem: PropTypes.func.isRequired,
 };
 
 export default ShoppingCart;
