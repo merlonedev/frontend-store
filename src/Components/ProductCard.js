f3afae2e575d273d9187e7876ff53f9c7fec2b07
Gabriel Viana <g.viana87@hotmail.com>
2021-07-15 13:54:43 -0300

    add Link para page ProductDetails na renderizaçao dos produtos ProductCard

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 
 class ProductCard extends Component {
   render() {
-    const { product: { title, thumbnail, price } } = this.props;
+    const { product: { id, title, thumbnail, price } } = this.props;
     return (
       <section data-testid="product">
         <h3>{ title }</h3>
         <img src={ thumbnail } alt={ title } />
         <p>{ price }</p>
+        <Link
+          data-testid="product-detail-link"
+          to={ `/productDetail/${id}` }
+        >
+          + Detalhes
+        </Link>
       </section>
     );
   }
@@ -16,6 +23,7 @@ class ProductCard extends Component {
 
 ProductCard.propTypes = {
   product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     thumbnail: PropTypes.string.isRequired,
     price: PropTypes.number.isRequired,
