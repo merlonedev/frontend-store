c04e61e3790f0861ea91473193e1f0f9802157e5
lucasmulato <cobain.lucas23@gmail.com>
2021-07-15 15:46:21 -0300

    Req 10, incompleto.

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,6 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.decreaseQtt = this.decreaseQtt.bind(this);
+    this.increaseQtt = this.increaseQtt.bind(this);
+  }
+
+  // Função para decrementar a quntidade
+  decreaseQtt() {
+    this.setState((previousState) => ({
+      quantity: previousState.quantity - 1,
+    }));
+  }
+
+  // Função para incrementar a quantidade
+  increaseQtt() {
+    this.setState((previousState) => ({
+      quantity: previousState.quantity + 1,
+    }));
+  }
+
   render() {
     const { itemCart } = this.props;
     return (
@@ -10,7 +31,23 @@ export default class ShoppingCart extends React.Component {
           <div key={ id }>
             <h3 data-testid="shopping-cart-product-name">{title}</h3>
             <img src={ thumbnail } alt="Imagem do produto" />
+            {/* Adicionando o botão de decrementação */}
+            <button
+              type="button"
+              onClick={ this.decreaseQtt }
+              data-testid="product-decrease-quantity"
+            >
+              -
+            </button>
             <p data-testid="shopping-cart-product-quantity">{quantity}</p>
+            {/* Adicionando o botão de incrementação */}
+            <button
+              type="button"
+              onClick={ this.increaseQtt }
+              data-testid="product-increase-quantity"
+            >
+              +
+            </button>
           </div>
         ))) : (
           <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
