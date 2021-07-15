cd8c2677c24bc5770f095ae22d9946987239b195
ladcs <hpladcs@gmail.com>
2021-07-15 13:20:40 -0300

    R6| arrumando lint

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Route } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import ShoppingCart from './components/ShoppingCart';
 import ProductList from './components/ProductList';
-import showCategory from './components/showCategory';
 
 class App extends Component {
   render() {
@@ -12,7 +11,6 @@ class App extends Component {
       <BrowserRouter>
         <Route exact path="/" component={ ProductList } />
         <Route exact path="/Shoppingcart" component={ ShoppingCart } />
-        <Route exact path="/categorias/:categoria" component={ showCategory } />
       </BrowserRouter>
     );
   }
