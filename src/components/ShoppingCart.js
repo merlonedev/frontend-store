0f7e16cd26fdf42201d0ebe20bb90b49b2d8da8e
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-15 19:37:48 -0300

    Mudanca para trackear via estado

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -5,53 +5,79 @@ class ShoppingCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      size: 1,
+      items: '',
+      tracker: false,
     };
     this.decreaseQtt = this.decreaseQtt.bind(this);
     this.increaseQtt = this.increaseQtt.bind(this);
+    this.loadState = this.loadState.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadState();
+  }
+
+  loadState() {
+    const { itemCart } = this.props;
+    const { items } = this.state;
+    const infos = itemCart.map((item) => {
+      const obj = {
+        id: item.id,
+        title: item.title,
+        quantity: item.quantity,
+        thumbnail: item.thumbnail,
+        price: item.price,
+      };
+      return obj;
+    });
+    this.setState({
+      items: [...items, infos],
+    });
   }
 
   // Função para decrementar a quntidade
-  decreaseQtt() {
-    const { itemCart } = this.props;
+  decreaseQtt(id) {
+    const { items, tracker } = this.state;
+    const value = items[0].find((item) => item.id === id);
+    value.quantity -= 1;
     this.setState({
-      size: itemCart.length -= 1,
+      tracker: !tracker,
     });
   }
 
   // Função para incrementar a quantidade
-  increaseQtt(event) {
-    const { itemCart } = this.props;
-    console.log(itemCart);
-    console.log(event.target);
+  increaseQtt(id) {
+    const { items, tracker } = this.state;
+    const value = items[0].find((item) => item.id === id);
+    value.quantity += 1;
     this.setState({
-      size: itemCart.length += 1,
+      tracker: !tracker,
     });
   }
 
   render() {
-    const { size } = this.state;
-    const { itemCart } = this.props;
+    const { items } = this.state;
+    const abc = localStorage.getItem('count');
     return (
       <div>
-        {itemCart.length ? (itemCart.map(({ title, id, thumbnail }) => (
+        {abc && items.length ? (items[0].map(({ title, id, thumbnail, quantity }) => (
           <div key={ id }>
             <h3 data-testid="shopping-cart-product-name">{title}</h3>
             <img src={ thumbnail } alt="Imagem do produto" />
             {/* Adicionando o botão de decrementação */}
             <button
               type="button"
-              onClick={ this.decreaseQtt }
+              onClick={ () => this.decreaseQtt(id) }
               data-testid="product-decrease-quantity"
             >
               -
             </button>
-            <p data-testid="shopping-cart-product-quantity">{size}</p>
+            <p data-testid="shopping-cart-product-quantity">{quantity}</p>
             {/* Adicionando o botão de incrementação */}
             <button
               type="button"
               value={ id }
-              onClick={ this.increaseQtt }
+              onClick={ () => this.increaseQtt(id) }
               data-testid="product-increase-quantity"
             >
               +
