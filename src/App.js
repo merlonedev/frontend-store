73f987c59c4b11f1208633d6c7bc3f7633f9ebf1
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-07-16 11:03:12 -0300

    refatorando as funçoes de botao ShoppingCart para App, para melhor manipulaçao dos estados

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
 import ProductDetails from './components/ProductDetails';
 import CategoryPage from './components/CategoryPage';
+import FinishPage from './components/FinishPage';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { cartItens: [] };
+    this.state = { cartItens: [], totalQuantity: 0 };
     this.cartAdd = this.cartAdd.bind(this);
     this.removeItem = this.removeItem.bind(this);
+    this.quantitySum = this.quantitySum.bind(this);
+    this.quantitySub = this.quantitySub.bind(this);
   }
 
   cartAdd(product) {
     this.setState((prevState) => ({
       cartItens: [...prevState.cartItens, product],
+      totalQuantity: prevState.cartItens.length + 1,
     }));
   }
 
@@ -26,6 +30,23 @@ class App extends Component {
       const delFilter = cartItens.filter((cartItem) => cartItem.id !== product);
       return { cartItens: delFilter };
     });
+  }
+
+  quantitySum(id) {
+    this.setState((prevState) => ({
+      [id]: prevState[id] ? prevState[id] + 1 : 2,
+      totalQuantity: prevState.totalQuantity + 1,
+    }));
+  }
+
+  quantitySub(id, quantidade) {
+    if (quantidade === 1) {
+      return;
+    }
+    this.setState((prevState) => ({
+      [id]: prevState[id] ? prevState[id] - 1 : 1,
+      totalQuantity: prevState.totalQuantity === 1 ? 1 : prevState.totalQuantity - 1,
+    }));
   }
 
   render() {
@@ -44,6 +65,9 @@ class App extends Component {
             render={ (props) => (<ShoppingCart
               { ...props }
               removeItem={ this.removeItem }
+              quantitySum={ this.quantitySum }
+              quantitySub={ this.quantitySub }
+              estado={ this.state }
               cartAdd={ cartItens }
             />) }
           />
@@ -62,6 +86,11 @@ class App extends Component {
               { ...props }
               cartAdd={ this.cartAdd }
             />) }
+          />
+          <Route
+            exact
+            path="/finalizarcompra"
+            component={ FinishPage }
           />
         </Switch>
       </BrowserRouter>
---
+++ src/components/FinishPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FinishPage extends Component {
+  render() {
+    return (
+      <div>
+        Finalizei Compra
+      </div>
+    );
+  }
+}
+
+export default FinishPage;
