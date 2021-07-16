6e9652b87f6c993540276a15c81838b25423431e
Carlos Eduardo Tomé da Silva <caduts10@gmail.com>
2021-07-15 22:20:47 -0300

    Tentativa Correção Soma Total do Carrinho

--- src/Components/CartItemProduct.js
+++ src/Components/CartItemProduct.js
@@ -4,101 +4,40 @@ import PropTypes from 'prop-types';
 export default class CartItemProduct extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       clicks: 1,
-      price: 0,
-      totalPrice: 0,
     };
-
-    this.totalPrice = this.totalPrice.bind(this);
-    this.minusItem = this.minusItem.bind(this);
-    this.minusTotalPrice = this.minusTotalPrice.bind(this);
-    this.plusItem = this.plusItem.bind(this);
-    this.removeFromCart = this.removeFromCart.bind(this);
   }
 
-  componentDidMount() {
-    this.totalPrice();
-  }
-
-  totalPrice() {
-    const { clicks, price } = this.state;
-    this.setState({ totalPrice: (price * clicks),
-    });
-  }
-
-  plusItem() {
-    const { price } = this.state;
-    this.setState((state) => ({
-      clicks: state.clicks + 1,
-    }), () => {
-      this.totalPrice(price);
-    });
-  }
-  // talvez tenha que usar um onchange
-
-  minusItem() {
-    const { clicks } = this.state;
-    if (clicks === 0 || clicks > 1) {
-      this.setState({ clicks: clicks - 1 });
-    }
-    this.totalPrice();
-  }
-
-  minusTotalPrice() {
-
-  }
-
-  removeFromCart() {
-    localStorage.clear();
-  }
-
-  /* this.setState((state) => ({
-    product: state.product.filter((product) => product.id !== id),
-  })); */
   render() {
-    const { product } = this.props;
-    const { title, thumbnail, price, id } = product;
-    const { totalPrice, clicks } = this.state;
+    const { clicks } = this.state;
+    const { product, handlePlusButton, handleMinusButton } = this.props;
+    const { title, thumbnail, price } = product;
     return (
-      <>
-        <li key={ title }>
-          <button
-            type="button"
-            onClick={ this.removeFromCart }
-          >
-            X
-          </button>
-          <div data-testid="product">
-            <p data-testid="shopping-cart-product-name">{title}</p>
-            <img src={ thumbnail } alt={ title } />
-            <p>{price}</p>
-          </div>
+      <li key={ title }>
+        <div data-testid="product">
+          <p data-testid="shopping-cart-product-name">{title}</p>
+          <img src={ thumbnail } alt={ title } />
+          <p>{price}</p>
           <h2 data-testid="shopping-cart-product-quantity">{ clicks }</h2>
           <button
             data-testid="product-increase-quantity"
             type="button"
-            onClick={ () => {
-              this.plusItem(id);
-              this.totalPrice(price);
-            } }
+            onClick={ handlePlusButton }
           >
             +
           </button>
           <button
             data-testid="product-decrease-quantity"
             type="button"
-            onClick={ () => {
-              this.minusItem(id);
-              this.minusTotalPrice(price, clicks);
-            } }
+            onClick={ handleMinusButton }
           >
             -
           </button>
           <p>{ `R$${(price * clicks).toFixed(2)}` }</p>
-        </li>
-        <h3>{`Valor Total da Compra: R$${totalPrice.toFixed(2)}`}</h3>
-      </>
+        </div>
+      </li>
     );
   }
 }
@@ -112,4 +51,6 @@ CartItemProduct.propTypes = {
     price: PropTypes.number,
     id: PropTypes.string.isRequired,
   }).isRequired,
+  handlePlusButton: PropTypes.func.isRequired,
+  handleMinusButton: PropTypes.func.isRequired,
 };
