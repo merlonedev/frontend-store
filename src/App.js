a650619c2dc1cf1a2d682af4c98c4eb591bf7d1b
Jossany Moura <mjossany@gmail.com>
2021-07-17 17:29:54 -0300

    Refatora requisito 10

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import ShoppingCart from './Components/ShoppingCart';
 import SearchBar from './Components/SearchBar';
 import InicialMessage from './Components/InicialMessage';
 import ProductDetail from './Components/ProductDetail';
-import Checkout from './Components/Checkout';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,20 +15,21 @@ class App extends React.Component {
     this.state = {
       category: '',
       search: '',
-      itemList: [],
       cartList: [],
     };
     this.getState = this.getState.bind(this);
     this.setCartStorage = this.setCartStorage.bind(this);
     this.setQuantity = this.setQuantity.bind(this);
-    this.addQuantity = this.addQuantity.bind(this);
     this.removeItem = this.removeItem.bind(this);
   }
 
   setCartStorage(obj) {
-    this.setState((previousState) => ({
-      itemList: [...previousState.itemList, obj],
-    }), () => this.addQuantity());
+    const { id } = obj;
+    const { cartList } = this.state;
+    const includes = cartList.find((item) => item.id === id);
+    if (includes) return this.setQuantity(includes.quantity + 1, id);
+    obj.quantity = 1;
+    this.setState((prevState) => ({ cartList: [...prevState.cartList, obj] }));
   }
 
   getState(name, value) {
@@ -48,22 +48,11 @@ class App extends React.Component {
     });
   }
 
-  addQuantity() {
-    const { itemList } = this.state;
-    const cartList = itemList.reduce((list, item) => {
-      const includes = list.some(({ id }) => item.id === id);
-      if (includes) return list;
-      item.quantity = itemList.filter(({ id }) => id === item.id).length;
-      return [...list, item];
-    }, []);
-    this.setState({ cartList });
-  }
-
   removeItem(idItem) {
     this.setState((prev) => {
-      const clearList = prev.itemList.filter(({ id }) => id !== idItem);
-      return { itemList: clearList };
-    }, () => this.addQuantity());
+      const clearList = prev.cartList.filter(({ id }) => id !== idItem);
+      return { cartList: clearList };
+    });
   }
 
   render() {
@@ -71,47 +60,42 @@ class App extends React.Component {
     return (
       <div>
         <BrowserRouter>
-          <header>
-            <SearchBar getState={ this.getState } />
-            <ShoppingCartButton />
-          </header>
-          <main>
-            <FilterCategories getState={ this.getState } />
-            <Switch>
-              <Route exact path="/" component={ InicialMessage } />
-              <Route
-                exact
-                path="/shopping-cart"
-                render={ () => (<ShoppingCart
-                  cartList={ cartList }
-                  removeItem={ this.removeItem }
-                  setQuantity={ this.setQuantity }
+          <ShoppingCartButton />
+          <SearchBar getState={ this.getState } />
+          <Switch>
+            <Route exact path="/" component={ InicialMessage } />
+            <Route
+              exact
+              path="/shopping-cart"
+              render={ () => (<ShoppingCart
+                cartList={ cartList }
+                removeItem={ this.removeItem }
+                setQuantity={ this.setQuantity }
+              />) }
+            />
+            <Route
+              exact
+              path="/search"
+              render={ (props) => (
+                <SearchResults
+                  { ...props }
+                  category={ category }
+                  search={ search }
+                  setCartStorage={ this.setCartStorage }
                 />) }
-              />
-              <Route
-                exact
-                path="/search"
-                render={ (props) => (
-                  <SearchResults
-                    { ...props }
-                    category={ category }
-                    search={ search }
-                    setCartStorage={ this.setCartStorage }
-                  />) }
-              />
-              <Route
-                exact
-                path="/details/:id"
-                render={ (props) => (
-                  <ProductDetail
-                    { ...props }
-                    search={ search }
-                    setCartStorage={ this.setCartStorage }
-                  />) }
-              />
-              <Route path="/shopping-cart/checkout" component={ Checkout } />
-            </Switch>
-          </main>
+            />
+            <Route
+              exact
+              path="/details/:id"
+              render={ (props) => (
+                <ProductDetail
+                  { ...props }
+                  search={ search }
+                  setCartStorage={ this.setCartStorage }
+                />) }
+            />
+          </Switch>
+          <FilterCategories getState={ this.getState } />
         </BrowserRouter>
       </div>
     );
@@ -121,3 +105,4 @@ class App extends React.Component {
 export default App;
 
 // Na refatoração realizada nesse código depois do requisito 6 consultei o repositório do Roberval Filho da Turma 12 Grupo 18. Lembrando que os testes do 1 ao 6 já estavam passando antes dessa refatoração. https://github.com/tryber/sd-12-project-frontend-online-store/pull/226/files
+// Na implementação do requisito 10 utilizamos a ideia e o código do Grupo 10 da turma 12. Segue link para o repositório https://github.com/tryber/sd-12-project-frontend-online-store/tree/main-group-10/src
