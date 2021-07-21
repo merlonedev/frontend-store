7b2ee29bc4a6ea51b3466a9bf7e4396e048f1a64
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-07-21 12:20:44 -0300

    Adiciona Componente SortProductList

---
+++ src/components/SortProductList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class SortProductList extends Component {
+  render() {
+    return (
+      <div>
+        sortProductList
+      </div>
+    );
+  }
+}
+
+export default SortProductList;
