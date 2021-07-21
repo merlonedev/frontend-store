885f273a3044bdfbc87bc5008eecaf4489ffbecc
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-07-21 12:24:46 -0300

    Retira react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
-import ShoppingCart from './components/ShoppingCart';
 import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Route exact path="/ShoppingCart" component={ ShoppingCart } />
-        <Route exact path="/" component={ Home } />
-      </BrowserRouter>
+      <Home />
     );
   }
 }
