571494d4bf01bf4e886a2b2a03ba2b0b58d3cef0
MZTBTG <mztbtg@hotmail.com>
2021-07-25 21:45:01 -0300

    Exercícios 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import SearchBar from './Components/SearchBar';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ShoppingCartButton from './Components/ShoppingCartButton';
+import ShoppingCart from './Components/ShoppingCart';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <SearchBar />
-      </div>
+      <BrowserRouter>
+        <ShoppingCartButton />
+        <Switch>
+          <Route path="/shopping-cart" component={ ShoppingCart } />
+          <Route exact path="/" component={ SearchBar } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
