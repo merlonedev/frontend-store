3ec607bf3df218ac57c3176f38e12d453d22bf28
victordg00 <victord.goncalves@outlook.com>
2021-08-02 17:50:49 -0300

    req 1 feito

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
   // Implemente aqui
+  let categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  categories = categories.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  let products = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  products = products.json();
+  return products;
 }
