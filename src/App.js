db001d7bc6235ad28bdd6c2cf8e2be034f222204
MZTBTG <mztbtg@hotmail.com>
2021-08-02 13:00:48 -0300

    Exercícios 5 e 6: Concluidos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import SearchBar from './Components/SearchBar';
 import FilterCategories from './Components/FilterCategories';
 import './App.css';
 import ShoppingCartButton from './Components/ShoppingCartButton';
 import ShoppingCart from './Components/ShoppingCart';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: '',
+    };
+    this.getCategory = this.getCategory.bind(this);
+  }
+
+  getCategory(name, value) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { category } = this.state;
     return (
       <div>
-        <SearchBar />
-        <FilterCategories />
         <BrowserRouter>
           <ShoppingCartButton />
           <Switch>
+            <Route
+              exact
+              path="/"
+              render={ (props) => (
+                <SearchAndResults { ...props } category={ category } />) }
+            />
             <Route path="/shopping-cart" component={ ShoppingCart } />
-            <Route exact path="/" component={ SearchBar } />
           </Switch>
+          <FilterCategories getCategory={ this.getCategory } />
         </BrowserRouter>
       </div>
     );
--- src/Components/FilterCategories.jsx
+++ src/Components/FilterCategories.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import * as api from '../services/api';
 
 class FilterCategories extends Component {
@@ -9,6 +10,7 @@ class FilterCategories extends Component {
     };
 
     this.getApi = this.getApi.bind(this);
+    this.clickHandle = this.clickHandle.bind(this);
   }
 
   componentDidMount() {
@@ -22,25 +24,36 @@ class FilterCategories extends Component {
     });
   }
 
+  clickHandle({ target: { name, id } }) {
+    const { getCategory } = this.props;
+    getCategory(name, id);
+  }
+  
   render() {
     const { categories } = this.state;
     return (
       <aside>
-        <ul>
-          {
-            categories.map((categorie) => (
-              <li
-                key={ categorie.id }
-                data-testid="category"
-              >
-                { categorie.name }
-              </li>
-            ))
-          }
-        </ul>
+        {
+          categories.map((categorie) => (
+            <button
+              type="button"
+              name="category"
+              key={ categorie.id }
+              id={ categorie.id }
+              data-testid="category"
+              onClick={ this.clickHandle }
+            >
+              { categorie.name }
+            </button>
+          ))
+        }
       </aside>
     );
   }
 }
+
+FilterCategories.propTypes = {
+  getCategory: PropTypes.func.isRequired,
+};
 
 export default FilterCategories;
