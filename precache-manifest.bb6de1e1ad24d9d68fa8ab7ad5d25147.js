bb8fef9bfb0861f7100e45e5a4d48cad8863dc36
merlone <alestori7@gmail.com>
2021-10-03 11:58:14 -0300

    Updates

---
+++ precache-manifest.bb6de1e1ad24d9d68fa8ab7ad5d25147.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3256071c37da21405096bf857f0329a",
+    "url": "/frontend-store/index.html"
+  },
+  {
+    "revision": "de03786cc812b3c6165c",
+    "url": "/frontend-store/static/css/main.34e14d01.chunk.css"
+  },
+  {
+    "revision": "8f5a2c561a502be8801f",
+    "url": "/frontend-store/static/js/2.8e2a1a10.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-store/static/js/2.8e2a1a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de03786cc812b3c6165c",
+    "url": "/frontend-store/static/js/main.32593ec3.chunk.js"
+  },
+  {
+    "revision": "fde0c3f6d3232004af21",
+    "url": "/frontend-store/static/js/runtime-main.80ae7912.js"
+  },
+  {
+    "revision": "64a255b0f9ac36d7385fe4ecca35478e",
+    "url": "/frontend-store/static/media/LandasansMedium-ALJ6m.64a255b0.otf"
+  },
+  {
+    "revision": "89abd579fe417dc4397bdb09b8e88cad",
+    "url": "/frontend-store/static/media/TipoPress-DgaW.89abd579.ttf"
+  },
+  {
+    "revision": "9217ca3896a40b661d65fe3a3603d683",
+    "url": "/frontend-store/static/media/elo.9217ca38.png"
+  },
+  {
+    "revision": "aee1c181892814a8352e03fd4b0e0207",
+    "url": "/frontend-store/static/media/faustao-errou.aee1c181.gif"
+  },
+  {
+    "revision": "545819e1ef7017a8b514847eab90ba37",
+    "url": "/frontend-store/static/media/faustao.545819e1.gif"
+  },
+  {
+    "revision": "4c4f5d3be13c4964d29c681164ff8a21",
+    "url": "/frontend-store/static/media/master.4c4f5d3b.png"
+  },
+  {
+    "revision": "2c23636a3472c42be89f7aedf221d070",
+    "url": "/frontend-store/static/media/se_vira_logo.2c23636a.png"
+  },
+  {
+    "revision": "f7d8cd98636377b4cbce9c6c69ac5dea",
+    "url": "/frontend-store/static/media/visa.f7d8cd98.png"
+  }
+]);
